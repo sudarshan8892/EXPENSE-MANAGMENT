9acbf03bea8b30c8a29c3c63c7917a7d9a2a8f72
mforazure9@gmail.com <mforazure9@gmail.com>
2023-08-06 18:43:03 +0530

    Add project files.

---
+++ ExpinseMangament/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExpinseMangament.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
