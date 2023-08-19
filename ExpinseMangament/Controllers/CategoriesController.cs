11e82b9fc7962673d15d2d25ebd5ed174524df28
mforazure9@gmail.com <mforazure9@gmail.com>
2023-08-19 21:45:23 +0530

    new

--- ExpinseMangament/Controllers/CategoriesController.cs
+++ ExpinseMangament/Controllers/CategoriesController.cs
@@ -81,7 +81,7 @@ namespace ExpinseMangament.Controllers
             return View( category);
         }
 
-        // GET: Categories/Edit/5
+       // GET: Categories/Edit/5
         //public async Task<IActionResult> Edit(int? id)
         //{
         //    if (id == null || _context.Categories == null)
@@ -133,24 +133,24 @@ namespace ExpinseMangament.Controllers
         //}
 
         // GET: Categories/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null || _context.Categories == null)
-        //    {
-        //        return NotFound();
-        //    }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
 
-        //    var category = await _context.Categories
-        //        .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (category == null)
-        //    {
-        //        return NotFound();
-        //    }
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
 
-        //    return View(category);
-        //}
+            return View(category);
+        }
 
-       // POST: Categories/Delete/5
+        // POST: Categories/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
